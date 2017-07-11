8f1837b4c67f7233e5dd3bc73c30e679d3b046ee
Scott Lagler <laglerscott@gmail.com>
2017-07-10 21:31:19 -0500

    Created Menu Prompt

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
 import java.util.Scanner;
 
 /* To-Do List - project idea: https://goo.gl/PK2erS
-//TODO Create Menu System
 //TODO Make Database class
 //TODO Output/Input student list to/from a file
 */
 
 public class Main {
 	public static void main(String[] args) {
+		Scanner inpt = new Scanner(System.in);
 
+		System.out.println("Welcome to The University Student Manager.");
+		int response = 0;
+
+		while (!(response == 6)) {
+			System.out.println("Main Menu:");
+			System.out.println("  1. Add Student Record");
+			System.out.println("  2. Remove Student Record");
+			System.out.println("  3. List all Student Records"); //TODO add next page option
+			System.out.println("  4. Export Student Records");
+			System.out.println("  5. Import Student Records");
+			System.out.println("  6. Exit");
+			System.out.print("Please select a menu option: ");
+			response = inpt.nextInt();
+		}
 	}
 }
